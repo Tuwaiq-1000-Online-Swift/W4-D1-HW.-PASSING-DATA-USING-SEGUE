1a8a7f88ad257e29c94c9a5199e936adc1c6afec
Yarob Al Mostafa (يعرُب) <yarob.hm@gmail.com>
2021-11-22 16:11:02 +0300

    Initial Commit

---
+++ W4-D1-HW-Moving data using segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  W4-D1-HW-Moving data using segue
+//
+//  Created by يعرُب on 17/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
